604a9686d398636094fabc926fb5da192a4bc047
Fabio Coelho <fccoelho7@gmail.com>
2022-04-02 11:52:05 -0300

    add button group component

--- src/types.ts
+++ src/types.ts
@@ -1,8 +1,3 @@
 import { NullstackClientContext } from "nullstack/types";
 
-export interface ButtonProps {
-  class?: string;
-  onClick?: (ctx: NullstackClientContext) => void;
-}
-
 export interface ApplicationClientContext extends NullstackClientContext {}
